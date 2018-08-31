2fee64a7c4a1b023f8a156ba96b80c05e1f051ef
Serhiy K <mrlanu@gmail.com>
2018-08-30 20:18:43 -0500

    injecting & using training.service

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {TrainingService} from '../training.service';
+import {Exercise} from '../exercise.model';
 
 @Component({
   selector: 'app-new-training',
@@ -9,9 +11,12 @@ export class NewTrainingComponent implements OnInit {
 
   @Output() trainingStart = new EventEmitter<void>();
 
-  constructor() { }
+  availableExercise: Exercise[] = [];
+
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
+    this.availableExercise = this.trainingService.getAvailableExercises();
   }
 
   onStartTraining(){
