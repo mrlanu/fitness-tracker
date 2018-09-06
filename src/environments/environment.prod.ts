a9d9d00a07f041db2f48b0c0a787b5cdb1ae64fb
Serhiy K <mrlanu@gmail.com>
2018-09-05 22:08:25 -0500

    environment for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD7Z4lX3Abmsh9ZjRJXvgG1VfiOV4xfTDE",
+    authDomain: "ng-fitness-tracker-af764.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-af764.firebaseio.com",
+    projectId: "ng-fitness-tracker-af764",
+    storageBucket: "ng-fitness-tracker-af764.appspot.com",
+    messagingSenderId: "105564604523"
+  }
 };
